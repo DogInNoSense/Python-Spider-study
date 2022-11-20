4c0703582506a7271dd006ec7bb41939186f9814
xyz <xieyinzhangl@gmail.com>
2022-11-20 18:02:42 +0800

    first commit

---
+++ 008_爬虫__变量的定义.py
@@ -0,0 +1,19 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2022/11/20 17:56
+# @Author : www.lmxyz.xyz
+# @File : 008_爬虫__变量的定义
+# @Project : spider-study
+
+# 在使用变量的情况下
+# 变量的格式: 变量的名字 = 变量的值
+
+weather = '今天的天气非常好!'
+print(weather)
+print(weather)
+print(weather)
+print(weather)
+
+# 应用情景
+
+img = 'https://www.lmxyz.xyz/hexo-setup/1.png'
+print(img)
