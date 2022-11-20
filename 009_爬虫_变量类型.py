da9367521043def74466851c489c4ec02593ac9f
xyz <xieyinzhangl@gmail.com>
2022-11-20 20:27:48 +0800

    first commit

---
+++ 009_爬虫_变量类型.py
@@ -0,0 +1,38 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2022/11/20 20:18
+# @Author : www.lmxyz.xyz
+# @File : 009_爬虫_变量类型
+# @Project : spider-study
+
+
+# int
+# float
+# boolean
+# string
+# list 列表
+# tuple 元组
+# dict 字典
+
+
+# 基本使用
+
+# Number 数值
+# int
+money = 5000
+# float
+money1 = 3.5
+# 男 True 女 False
+sex = True
+gender = False
+
+# string字符串 单引号或双引号
+s = '我是字符串'
+s1 = "我是字符串"
+
+# 单引号和双引号的嵌套
+s4 = '"中国"'
+print(s4)
+s5 = "'中国'"
+print(s5)
+
+# 不允许 单引号套单引号,双引号套双引号
